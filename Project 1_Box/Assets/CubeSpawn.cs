11f3b8141a280bf3da8853c46dca91ee023891f0
sarahkling <Sarah@gromit134-183.hampshire.edu>
2015-09-23 18:25:23 -0400

    Publishing Box to Git
    Yay

---
+++ Project 1_Box/Assets/CubeSpawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpawn : MonoBehaviour {
+	public GameObject cube;
+	bool cubeHasSpawned = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > 3 && cubeHasSpawned == false){
+			Instantiate(cube, new Vector3(0,0,0), Quaternion.identity);
+			cubeHasSpawned = true;
+		}
+		
+	}
+}
